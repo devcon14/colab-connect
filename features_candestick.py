eeb0a83dbbe3cfa542d0eabf5f8d2bfc33c3a546
devcon14 <support@mobilescanning.co.za>
2022-11-06 15:51:44 +0200

    Update features_candestick.py

--- features_candestick.py
+++ features_candestick.py
@@ -15,6 +15,29 @@ def get_features_cnd(sec):
     sec[f"ft_cnd_nr_{period}"] = sec["ft_cnd_range"] == sec["ft_cnd_range"].rolling(period).min()
     sec[f"ft_cnd_wr_{period}"] = sec["ft_cnd_range"] == sec["ft_cnd_range"].rolling(period).max()
   sec
+  
+  # %%
+  sec["ft_cnd_high_breakout"] = sec["high"] > sec["high"].shift(1)
+  sec["ft_cnd_low_breakout"] = sec["low"] < sec["low"].shift(1)
+  # kaggle convention was ft_can_lower_shadow...
+  # could try auto prefix all columns
+  sec["ft_cnd_lower_shadow"] = sec.query("high != low").apply(lambda x: (min(x["open"], x["close"])-x["low"]) / (x["high"] - x["low"]), axis="columns")
+  sec["ft_cnd_upper_shadow"] = sec.query("high != low").apply(lambda x: (x["high"]-max(x["open"], x["close"])) / (x["high"] - x["low"]), axis="columns")
+
+  sec["ft_cnd_closing_high"] = sec["close"] > sec["high"].shift(1)
+  sec["ft_cnd_closing_low"] = sec["close"] < sec["low"].shift(1)
+  sec["ft_cnd_closing_high_1"] = sec["ft_cnd_closing_high"].shift(1)
+  sec["ft_cnd_closing_low_1"] = sec["ft_cnd_closing_low"].shift(1)
+
+  # the strat
+  sec["tmp_high_1"] = sec["high"].shift(1)
+  sec["tmp_low_1"] = sec["low"].shift(1)
+  sec["ft_can_inside_bar"] = (sec["high"] < sec["tmp_high_1"]) & (sec["low"] > sec["tmp_low_1"])
+  # engulfing / outside bar
+  sec["ft_can_engulfing"] = (sec["high"] > sec["tmp_high_1"]) & (sec["low"] < sec["tmp_low_1"])
+  sec["ft_can_green_soldier"] = (sec["high"] > sec["tmp_high_1"]) & (sec["low"] > sec["tmp_low_1"])
+  sec["ft_can_red_soldier"] = (sec["high"] < sec["tmp_high_1"]) & (sec["low"] < sec["tmp_low_1"])
+  
 
 def get_features_pa(sec):
   # %%
