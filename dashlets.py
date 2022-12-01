7ce02bfd34ab440577a1112352b4978d8c604fcf
devcon14 <support@mobilescanning.co.za>
2022-12-01 20:57:04 +0200

    Update dashlets.py

--- dashlets.py
+++ dashlets.py
@@ -2,7 +2,7 @@
 try:
   import hvplot.pandas
 except:
-  !pip install hvplot
+  # !pip install hvplot
   import hvplot.pandas
 
 import holoviews as hv
@@ -13,6 +13,25 @@ import pandas as pd
 pn.extension()
 hv.extension("bokeh")
 
+def get_viz_features(sec):
+    # must apply with 1 symbol
+    sec["change"] = sec["close"].pct_change()
+
+    # can apply with all
+    sec_all = sec
+    sec_all["date"] = pd.to_datetime(sec_all.index)
+
+    # -- YM
+    sec_all["ts_dt_YM"] = sec_all["date"].dt.strftime("%Y:%m")
+    # -- doy
+    # sec_all[f"ts_dt_doy"] = sec_all["date"].dt.dayofyear
+    sec_all[f"ts_dt_doy"] = sec_all["date"].dt.strftime("%m:%b:%d")
+    # -- dom
+    sec_all["ts_dt_dom"] = sec_all["date"].dt.day
+    # -- dow
+    # sec_all.loc[:, f"ts_dt_dow"] = sec_all["date"].dt.dayofweek
+    sec_all[f"ts_dt_dow"] = sec_all["date"].dt.strftime("%w:%a")
+
 def add_data(symbols, drill_down, chart_type):
   global sec, sec_dict, sec_all
   for symbol in symbols:
@@ -20,27 +39,14 @@ def add_data(symbols, drill_down, chart_type):
       print (f"Loading {symbol}")
       sec = web.DataReader(symbol, "av-forex-daily", api_key=ALPHAVANTAGE_API)
       sec["symbol"] = symbol
-      sec["change"] = sec["close"].pct_change()
+      get_viz_features(sec)
+      sec = sec.sort_values("date")
       sec_dict[symbol] = sec
 
   sec_arr = sec_dict.values()
   sec_all = pd.concat(sec_arr)
 
   PREFIX = "ft_" # ts, plt, ft
-  sec_all["date"] = pd.to_datetime(sec_all.index)
-  # just update all with no elif?
-  if drill_down == "YM":
-    key = f"ts_dt_YM"
-    sec_all[key] = sec_all["date"].dt.strftime("%Y:%m")
-  elif drill_down == "doy":
-    # sec_all[f"ts_dt_doy"] = sec_all["date"].dt.dayofyear
-    sec_all[f"ts_dt_doy"] = sec_all["date"].dt.strftime("%m:%b:%d")
-  elif drill_down == "dom":
-    sec_all["ts_dt_dom"] = sec_all["date"].dt.day
-  elif drill_down == "dow":
-    sec_all.loc[:, f"ts_dt_dow"] = sec_all["date"].dt.dayofweek
-    sec_all[f"ts_dt_dow"] = sec_all["date"].dt.strftime("%w:%a")
-
   data_plt = sec_all.groupby(["symbol", f"ts_dt_{drill_down}"])["change"].mean()
   data_plt = data_plt.reset_index()
   data_plt["cumsum"] = data_plt.change.cumsum()
@@ -50,8 +56,7 @@ def add_data(symbols, drill_down, chart_type):
   else:
     return data_plt.hvplot.line(x=f"ts_dt_{drill_down}", y="cumsum", by="symbol")
 
-def instrument_selector():
-  currency_pairs = ["EUR/USD", "AUD/USD", "GBP/USD", "GBP/JPY"]
+def instrument_selector(currency_pairs = ["EUR/USD", "GBP/USD", "CHF/USD", "JPY/USD", "AUD/USD", "CAD/USD"], value=["EUR/USD"]):
 
   dashlet_select = interact(
       add_data,
@@ -62,4 +67,5 @@ def instrument_selector():
   return dashlet_select
 
 if __name__ == "__main__":
-  display(instrument_selector())
+  majors = ["EUR/USD", "GBP/USD", "CHF/USD", "JPY/USD", "AUD/USD", "CAD/USD"]
+  display(instrument_selector(majors, ["EUR/USD"]))
