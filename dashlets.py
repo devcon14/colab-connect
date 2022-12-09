57932d47e3092da0be83ab4f54b9914a1deb59b4
devcon14 <support@mobilescanning.co.za>
2022-12-09 22:46:40 +0200

    Update dashlets.py

--- dashlets.py
+++ dashlets.py
@@ -10,8 +10,6 @@ import panel as pn
 from panel import interact
 import pandas_datareader.data as web
 import pandas as pd
-pn.extension()
-hv.extension("bokeh")
 
 def get_viz_features(sec):
     # must apply with 1 symbol
@@ -23,6 +21,10 @@ def get_viz_features(sec):
 
     # -- YM
     sec_all["ts_dt_YM"] = sec_all["date"].dt.strftime("%Y:%m")
+    # -- cycles
+    sec_all["ts_dt_decennial"] = pd.Series(sec_all["date"].dt.year % 10).astype(str) + sec_all["date"].dt.strftime(":%m")
+    sec_all["ts_dt_shmita"] = pd.Series(sec_all["date"].dt.year % 7).astype(str) + sec_all["date"].dt.strftime(":%m")
+    sec_all["ts_dt_election"] = pd.Series(sec_all["date"].dt.year % 4).astype(str) + sec_all["date"].dt.strftime(":%m")
     # -- doy
     # sec_all[f"ts_dt_doy"] = sec_all["date"].dt.dayofyear
     sec_all[f"ts_dt_doy"] = sec_all["date"].dt.strftime("%m:%b:%d")
@@ -32,22 +34,26 @@ def get_viz_features(sec):
     # sec_all.loc[:, f"ts_dt_dow"] = sec_all["date"].dt.dayofweek
     sec_all[f"ts_dt_dow"] = sec_all["date"].dt.strftime("%w:%a")
 
-def add_data(symbols, drill_down, chart_type):
+def add_data(sec_inst, symbols, drill_down, chart_type):
   global sec, sec_dict, sec_all
   for symbol in symbols:
-    if not symbol in sec_dict.keys():
+    if not symbol in sec_inst.sec_dict.keys():
       print (f"Loading {symbol}")
-      sec = web.DataReader(symbol, "av-forex-daily", api_key=ALPHAVANTAGE_API)
+      if "/" in symbol:
+        sec = web.DataReader(symbol, "av-forex-daily", api_key=ALPHAVANTAGE_API)
+      else:
+        sec = web.DataReader(symbol, "av-daily-adjusted", api_key=ALPHAVANTAGE_API)
       sec["symbol"] = symbol
+      sec = sec.sort_index()
       get_viz_features(sec)
-      sec = sec.sort_values("date")
-      sec_dict[symbol] = sec
+      # sec = sec.sort_values("date")
+      sec_inst.sec_dict[symbol] = sec
 
-  sec_arr = sec_dict.values()
-  sec_all = pd.concat(sec_arr)
+  sec_inst.sec_arr = sec_inst.sec_dict.values()
+  sec_inst.sec_all = pd.concat(sec_inst.sec_arr)
 
   PREFIX = "ft_" # ts, plt, ft
-  data_plt = sec_all.groupby(["symbol", f"ts_dt_{drill_down}"])["change"].mean()
+  data_plt = sec_inst.sec_all.groupby(["symbol", f"ts_dt_{drill_down}"])["change"].mean()
   data_plt = data_plt.reset_index()
   data_plt["cumsum"] = data_plt.change.cumsum()
 
@@ -56,16 +62,31 @@ def add_data(symbols, drill_down, chart_type):
   else:
     return data_plt.hvplot.line(x=f"ts_dt_{drill_down}", y="cumsum", by="symbol")
 
-def instrument_selector(currency_pairs = ["EUR/USD", "GBP/USD", "CHF/USD", "JPY/USD", "AUD/USD", "CAD/USD"], value=["EUR/USD"]):
+def instrument_selector(sec_inst, currency_pairs = ["EUR/USD", "GBP/USD", "CHF/USD", "JPY/USD", "AUD/USD", "CAD/USD"], value=["EUR/USD"]):
 
   dashlet_select = interact(
       add_data,
+      sec_inst=sec_inst,
       symbols=pn.widgets.MultiChoice(options=currency_pairs, value=["EUR/USD"]),
-      drill_down=pn.widgets.Select(options=["YM", "doy", "dom", "dow"]),
+      drill_down=pn.widgets.Select(options=["YM", "decennial", "shmita", "election", "doy", "dom", "dow"]),
       chart_type=["line", "bar"]
       )
   return dashlet_select
 
+class Universe:
+  sec = None
+  sec_arr = []
+  sec_dict = {}
+  sec_all = None
+
 if __name__ == "__main__":
+  pn.extension(comms="colab")
+  # hv.extension("bokeh")
+
+  sec_inst = Universe()
+
   majors = ["EUR/USD", "GBP/USD", "CHF/USD", "JPY/USD", "AUD/USD", "CAD/USD"]
-  display(instrument_selector(majors, ["EUR/USD"]))
+  etfs = ["SPY", "GLD", "UUP", "SLV"]
+  # start_vals = ["EUR/USD", "GBP/USD", "CHF/USD"]
+  start_vals = ["GLD"]
+  display(instrument_selector(sec_inst, etfs, start_vals))
