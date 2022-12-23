40cb67b37400a9c56cfa5399db388c6a69b3a616
devcon14 <support@mobilescanning.co.za>
2022-12-23 16:37:50 +0200

    Update features_main.py

--- features_main.py
+++ features_main.py
@@ -1,1 +1,9 @@
 from features_candestick import get_features_cnd
+# 40in20out, volume spike
+from features_candlestick import get_features_ta as get_features_tmp_ta
+
+# win streaks, stats_ta, bizdays, dow_by_week
+from features_kaggle import get_features as get_features_kaggle
+
+# chart features, date features
+# def get_all_features
