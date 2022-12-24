6fbebbfe7f2fda900aea7ade0d5ab5e72ae24553
devcon14 <support@mobilescanning.co.za>
2022-12-24 17:06:49 +0200

    Create backtest_lib_market_entry.py

---
+++ backtest_lib_market_entry.py
@@ -0,0 +1,35 @@
+class MarketStrategy(Strategy):
+
+  def init(self):
+
+    def fn_signal_long(): return self.data.Signal_long
+    def fn_signal_short(): return self.data.Signal_short
+
+    self.is_signal_long = self.I(fn_signal_long, overlay=False)
+    self.is_signal_short = self.I(fn_signal_short, overlay=False)
+
+  def signal_exit(self):
+    if self.position:
+      
+      for trade in self.trades:
+        if trade.is_long:
+          if self.data.Signal_long_exit:
+            trade.close()
+        elif trade.is_short:
+          if self.data.Signal_short_exit:
+            trade.close()
+
+  def postprocess(self):
+    self.signal_exit()
+        
+  def next(self):
+
+    if not self.position:
+      
+      if self.data.Signal_long:
+        self.buy()
+      elif self.data.Signal_short:
+        self.sell()
+    
+    self.postprocess()
+
