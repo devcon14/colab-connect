5d0d38cbb96501719af5a16f4fab30199f20c80f
devcon14 <support@mobilescanning.co.za>
2022-12-25 21:18:23 +0200

    Update features_main.py

--- features_main.py
+++ features_main.py
@@ -1,6 +1,6 @@
 from features_candlestick import get_features_cnd
 # 40in20out, volume spike
-from features_candlestick import get_features_ta as get_features_tmp_ta
+from features_candlestick import get_features_ta as get_features_cnd_trend
 
 # win streaks, stats_ta, bizdays, dow_by_week
 from features_kaggle import get_features as get_features_kaggle
@@ -10,6 +10,7 @@ from features_kaggle import get_features as get_features_kaggle
 
 # %%
 def get_features_indicators(sec):
+  import pandas_ta as ta
   # sec["ft_ta_stoch_p14"] = ta.stoch(close=sec.close, high=sec.high, low=sec.low, length=14)["STOCHk_14_3_3"]
   sec["ft_ta_rsi_p2"] = ta.rsi(close=sec.close, length=2)
   sec["ft_ta_sma_p200"] = sec["close"].rolling(200).mean()
