01112e975880186f8492b56f42548292f549b1c8
devcon14 <support@mobilescanning.co.za>
2023-02-25 13:25:01 +0200

    Update features_backtest.py

--- features_backtest.py
+++ features_backtest.py
@@ -13,17 +13,26 @@ def get_prior_return_metrics(sec):
   sec["session_returns"] = sec["close"] - sec["open"]
   # today's open - yesterday's close
   sec["gap_returns"] = sec["open"] - sec["close"].shift(1)
+
+def prior_to_future_metrics(sec, future_horizon):
+  if future_horizon == 1:
+    suffix = ""
+  else:
+    suffix = f"_p{future_horizon}"
+    
+  sec[f"future_change_{suffix}"] = sec["change"].shift(-future_horizon)
+  sec[f"future_returns_{suffix}"] = sec["returns"].shift(-future_horizon)
   
-def get_backtest_metrics(sec, future_horizon = 5, adj=None):
+def backtest_metrics(sec, future_horizon = 5, adj=None):
   """
   Future returns for backtesting.
   """
-  sec["future_returns"] = sec["close"].shift(-future_horizon) - sec["close"]
+  get_prior_return_metrics(sec)
+  prior_to_future_metrics(sec, future_horizon)
+  
   if adj:
     sec["future_returns"] *= adj
 
-  sec["future_change"] = sec["close"].pct_change().shift(-1)
-    
   # ternary is for rolling window exceptions
   sec["mae"] = sec["close"].rolling(future_horizon).apply(lambda x: x[0] - np.min(x) if len(x) >= future_horizon else 0)
   sec["mae"] = sec["mae"].shift(-future_horizon)
@@ -34,14 +43,14 @@ def get_backtest_metrics(sec, future_horizon = 5, adj=None):
 
   return sec
 
-def get_backtest_metrics(sec_inst, future_horizon = 5, adj=None):
+def interactable_backtest_metrics(sec_inst, future_horizon = 5, adj=None):
   sec_tr = sec_inst.sec_all.groupby("symbol").apply(lambda x: get_backtest_metrics_on_df(x, future_horizon, adj))  
   sec_inst.sec_all = sec_tr
   return sec_tr[["long_mfe", "long_mae", "long_f2a"]].head(5)
 
 if __name__ == "__main__":
   adj = 1000
-  dashlet_backtest_metrics = interact(get_backtest_metrics, sec_inst=universe, future_horizon=(1, 20))
+  dashlet_backtest_metrics = interact(interactable_backtest_metrics, sec_inst=universe, future_horizon=(1, 20))
   display(dashlet_backtest_metrics)
 
-  # sec_inst.sec_all.groupby("symbol").apply(lambda x: get_backtest_metrics_on_df(x, 5))
+  # sec_inst.sec_all.groupby("symbol").apply(lambda x: backtest_metrics(x, 5))
